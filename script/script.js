667fdf2fed09cb7b3cea7b36f5b531c1fc4e3250
sayeed-cse <sayeed-web-dev@gmail.com>
2023-06-24 12:50:46 +0600

    final layout design for large screen

--- script/script.js
+++ script/script.js
@@ -59,12 +59,57 @@ const infiniteScroll = () => {
         carousel.scrollLeft = carousel.offsetWidth;
         carousel.classList.remove("no-transition");
     }
-
 }
-
-
 
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("scroll", infiniteScroll);
+
+
+// comments carousel
+
+const review_wrapper = document.querySelector(".review_wrapper");
+const carousel_comments = document.querySelector(".carousel_comments");
+const firstCardWidthCmt = carousel_comments.querySelector(".comments").offsetWidth;
+const arrowBtns_comments = document.querySelectorAll(".review_wrapper img");
+const carouselChild = [...carousel_comments.children];
+
+// Get the number of cards that can fit in the carousel at once
+let cardPerViewcmt = Math.round(carousel_comments.offsetWidth / firstCardWidthCmt);
+
+// Insert copies of the last few cards to beginning of carousel for infinite scrolling
+carouselChild.slice(-cardPerViewcmt).reverse().forEach(card => {
+    carousel_comments.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+
+// Insert copies of the first few cards to end of carousel for infinite scrolling
+carouselChild.slice(0, cardPerViewcmt).forEach(card => {
+    carousel_comments.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns_comments.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel_comments.scrollLeft += btn.id == "cmnt_left" ? -firstCardWidthCmt : firstCardWidthCmt;
+    });
+});
+
+
+
+const infiniteScrollCmt = () => {
+    // If the carousel is at the beginning, scroll to the end
+    if(carousel_comments.scrollLeft === 0) {
+        carousel_comments.classList.add("no-transition");
+        carousel_comments.scrollLeft = carousel_comments.scrollWidth - (2 * carousel_comments.offsetWidth);
+        carousel_comments.classList.remove("no-transition");
+    }
+    // If the carousel is at the end, scroll to the beginning
+    else if(Math.ceil(carousel_comments.scrollLeft) === carousel_comments.scrollWidth - carousel_comments.offsetWidth) {
+        carousel_comments.classList.add("no-transition");
+        carousel_comments.scrollLeft = carousel_comments.offsetWidth;
+        carousel_comments.classList.remove("no-transition");
+    }
+}
+
+carousel_comments.addEventListener("scroll", infiniteScrollCmt);
